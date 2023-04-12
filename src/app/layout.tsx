32973ba252037451a8041476e6e0e505a4de3e69
Ibrahim Loubani <ibrahimloubani2@gmail.com>
2023-04-12 17:15:44 +0300

    add authentication and toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Providers from '@/components/Providers'
 import './globals.css'
 
 export const metadata = {
@@ -11,8 +12,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+const Home = async () => {
   return (
     <div className='flex min-h-screen justify-center items-center w-full'>
       Main Index
     </div>
   )
 }
+
+export default Home
