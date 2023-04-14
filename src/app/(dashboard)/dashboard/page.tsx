e881d5697cdb669d001fd10cfad01ce865e44ea8
Ibrahim Loubani <ibrahimloubani2@gmail.com>
2023-04-14 18:26:30 +0300

    adding friend request and dashboard layout part 1

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,16 @@
 import Button from '@/components/ui/Button'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
 import React, { FC } from 'react'
 
 interface pageProps {}
 
-const page: FC<pageProps> = ({}) => {
+const page = async ({}) => {
+  const session = await getServerSession(authOptions)
   return (
     <div className='flex min-h-screen justify-center items-center w-full'>
       <Button>Submit</Button>
+      <pre>{JSON.stringify(session)}</pre>
     </div>
   )
 }
